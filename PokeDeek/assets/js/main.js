c41f3091eec6db924304412766ec8a11ad90537e
willamehenry <willamehenry1996@gmail.com>
2023-09-04 22:47:45 -0300

    subindo alteração 7.0

--- PokeDeek/assets/js/main.js
+++ PokeDeek/assets/js/main.js
@@ -1,17 +1,29 @@
-const offset = 0;
-const limit = 10;
+
+function convertPokemonLi(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}">
+<span class="number">#${pokemon.nunber}</span>
+<span class="nome">${pokemon.name}</span>
+
+<div class="detail">
+    <ol class="types">
+        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+    </ol>
+    <img src="${pokemon.photo}" 
+    alt="${pokemon.name}">
+
+</div>
+</li>`
 
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+}
 
-fetch (url).then (function(responde){
-console.log(responde);
+const pokemonList = document.getElementById('pokemonList');
+
+pokeApi.getPokemons().then((pokemonst = []) => {
+    const newhtml = pokemonst.map(convertPokemonLi).join('');
+
+    pokemonList.innerHTML = newhtml;
+
 
 })
-
-const x =10 +10;
-const y =20 + 20;
-const soma = (x*y)/2;
-
-
-console.log (soma);
